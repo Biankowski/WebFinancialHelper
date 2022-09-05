fe7c95ba90b1b39de26c2a1df54426f1db46a63b
Biankowski <rodrigobianchini96@gmail.com>
2022-09-05 14:40:45 -0300

    fixed merged

--- Models/CollectedData.cs
+++ Models/CollectedData.cs
@@ -10,15 +10,9 @@ namespace WebFinancialHelper.Models
         [Key]
         public int Id { get; set; }
         public string? PlaceOfPurchase { get; set; }
-<<<<<<< HEAD
         public decimal? Price { get; set; }
         public string? PurchaseDate { get; set; }
         public string? PurchaseTime { get; set; }
-=======
-        public decimal? Price { get; set; } 
-        public DateTime PurchaseDate { get; set; }
-        public string? PurchaseTime { get; set; } 
->>>>>>> 9a06fdb68958f87989c17946dd79a52162d72441
         public string? UploadDate { get; set; }
     }
 }
